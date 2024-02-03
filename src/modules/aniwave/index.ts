09448532f8548c11f87b450ec23ff6d02faf1f27
hoangvu12 <hggaming91@gmail.com>
2024-02-04 00:16:14 +0700

    fix(aniwave): add user agent to stream

--- src/modules/aniwave/index.ts
+++ src/modules/aniwave/index.ts
@@ -231,7 +231,18 @@ const anime: WindowAnime = {
 
     const m3u8File = await anime._getFilemoonLink(filemoonHTML);
 
-    container.videos = [{ file: { url: m3u8File } }];
+    container.videos = [
+      {
+        file: {
+          url: m3u8File,
+          headers: {
+            "user-agent":
+              "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 OPR/106.0.0.0",
+          },
+        },
+        format: "hls",
+      },
+    ];
 
     if (skipData) {
       try {
