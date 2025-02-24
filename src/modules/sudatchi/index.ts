c7646d4b0d1613a30892640b06545130ee816f5b
hoangvu12 <hggaming91@gmail.com>
2025-02-25 01:32:58 +0700

    fix(sudatchi): change api and stuff

--- src/modules/sudatchi/index.ts
+++ src/modules/sudatchi/index.ts
@@ -28,144 +28,61 @@ const anime: WindowAnime = {
   baseUrl: "https://sudatchi.com",
 
   getId: async ({ media }) => {
-    const searchResults = await anime._totalSearch(media);
-
-    const searchResultWithSameId = searchResults.find(
-      (result) => Number(result.extra?.anilistId) === media.id
-    );
-
-    if (searchResultWithSameId) {
-      return sendResponse({
-        data: searchResultWithSameId.id,
-        extraData: searchResultWithSameId.extra,
-      });
-    }
-
     sendResponse({
-      data: searchResults?.[0]?.id,
-      extraData: searchResults?.[0]?.extra,
+      data: media.id.toString(),
     });
   },
 
   getEpisodes: async ({ animeId }) => {
-    const { data: html } = await sendRequest<string>(
-      `${anime.baseUrl}/anime/${animeId}`
-    );
-
-    const props = anime._parseBetween(
-      html,
-      '<script id="__NEXT_DATA__" type="application/json">',
-      "</script>"
-    );
-
-    if (!props) return sendResponse([]);
-
-    const data = JSON.parse(props) as {
-      props: {
-        pageProps: {
-          animeData: {
-            id: number;
-            anilistId: number;
-            titleRomanji: string;
-            titleEnglish: string;
-            titleJapanese: string;
-            titleSpanish: any;
-            titleFilipino: any;
-            titleHindi: any;
-            titleKorean: any;
-            synonym: string;
-            synopsis: string;
-            slug: string;
-            statusId: number;
-            typeId: number;
-            year: number;
-            seasonId: number;
-            totalEpisodes: string;
-            seasonNumber: any;
-            imgUrl: string;
-            imgBanner: string;
-            trailerLink: string;
-            animeCrunchyId: string;
-            crunchyrollId: string;
-            hidiveId: any;
-            seasonHidiveId: any;
-            initialAirDate: string;
-            isAdult: boolean;
-            prequelId: any;
-            sequelId: any;
-            Status: {
-              id: number;
-              name: string;
-            };
-            Type: {
-              id: number;
-              name: string;
-            };
-            Season: {
-              id: number;
-              name: string;
-            };
-            characters: Array<{
-              id: number;
-              anilistId: number;
-              name: string;
-              role: string;
-              imageUrl: string;
-              animeId: number;
-              voiceActors: Array<{
-                id: number;
-                characterId: number;
-                voiceActorId: number;
-                voiceActor: {
-                  id: number;
-                  anilistId: number;
-                  name: string;
-                  language: string;
-                  imageUrl: string;
-                };
-              }>;
-            }>;
-            AnimeGenres: Array<{
-              animeId: number;
-              genreId: number;
-              Genre: {
-                id: number;
-                name: string;
-              };
-            }>;
-            Episodes: Array<{
-              id: number;
-              title: string;
-              number: number;
-              imgUrl: string;
-              animeId: number;
-              isProcessed: boolean;
-              openingStartsAt: number;
-              openingEndsAt: number;
-              _count: {
-                Subtitles: number;
-                AudioStreams: number;
-              };
-              releaseDate: any;
-              subtitleCount: number;
-              audioCount: number;
-            }>;
-            nextAirSchedule: {
-              id: number;
-              animeId: number;
-              episodeId: any;
-              episodeNumber: number;
-              airDate: string;
-            };
-          };
-        };
+    const { data: json } = await sendRequest<{
+      id: number;
+      title: {
+        english: string;
+        romaji: string;
+        native: string;
       };
-    };
+      episodesCount: number;
+      episodes: Array<any>;
+      startDate: {
+        year: number;
+        month: number;
+        day: number;
+      };
+      bannerImage: string;
+      coverImage: string;
+      logoImage: string;
+      description: string;
+      genres: Array<string>;
+      format: string;
+      season: string;
+      isAdult: boolean;
+      status: any;
+      nextAirSchedule: any;
+      otherSeasons: Array<{
+        id: number;
+        title: string;
+        season: string;
+        format: string;
+        coverImage: string;
+        bannerImage: any;
+        startYear: any;
+        relationType: string;
+      }>;
+      characters: Array<{
+        id: number;
+        name: string;
+        image: string;
+        role: string;
+      }>;
+      rating: string;
+      themes: Array<string>;
+      studio: string;
+      producers: Array<string>;
+    }>(`${anime.baseUrl}/api/anime/${animeId}`);
 
-    if (!data?.props?.pageProps?.animeData?.Episodes?.length)
-      return sendResponse([]);
+    const episodes = json.episodes;
 
-    const episodes = data.props.pageProps.animeData.Episodes;
+    if (!episodes?.length) return sendResponse([]);
 
     sendResponse(
       episodes.map((episode) => ({
@@ -228,125 +145,105 @@ const anime: WindowAnime = {
       file: { url: `${anime.baseUrl}/${json.url}` },
     });
 
-    const { data: html } = await sendRequest<string>(
-      `${anime.baseUrl}/watch/${animeId}/${number}`
-    );
-
-    const props = anime._parseBetween(
-      html,
-      '<script id="__NEXT_DATA__" type="application/json">',
-      "</script>"
-    );
-
-    if (!props) return sendResponse(container);
-
-    const data = JSON.parse(props) as {
-      props: {
-        pageProps: {
-          episodeData: {
-            anime: {
-              id: number;
-              titleRomanji: string;
-              titleEnglish: string;
-              titleJapanese: string;
-              synonym: string;
-              synopsis: string;
-              slug: string;
-              year: number;
-              isAdult: boolean;
-              totalEpisodes: number;
-              imgUrl: string;
-              imgBanner: string;
-              trailerLink: string;
-              Type: {
-                id: number;
-                name: string;
-              };
-              Status: {
-                id: number;
-                name: string;
-              };
-              Season: {
-                id: number;
-                name: string;
-              };
-              AnimeGenres: Array<{
-                Genre: {
-                  id: number;
-                  name: string;
-                };
-              }>;
-            };
-            currentEpisode: string;
-            episode: {
-              id: number;
-              title: string;
-              number: number;
-              imgUrl: string;
-              animeId: number;
-              isProcessed: boolean;
-              openingStartsAt: number;
-              openingEndsAt: number;
-              _count: {
-                EpisodeViews: number;
-              };
-              AudioStreams: Array<{
-                id: number;
-                episodeId: number;
-                languageId: number;
-                isDefault: boolean;
-                autoSelect: boolean;
-                playlistUri: string;
-              }>;
-            };
-            episodes: Array<{
-              id: number;
-              title: string;
-              number: number;
-              imgUrl: string;
-              animeId: number;
-              isProcessed: boolean;
-              openingStartsAt: number;
-              openingEndsAt: number;
-              _count: {
-                EpisodeViews: number;
-              };
-              AudioStreams: Array<{
-                id: number;
-                episodeId: number;
-                languageId: number;
-                isDefault: boolean;
-                autoSelect: boolean;
-                playlistUri: string;
-              }>;
-            }>;
-            previousEpisode: any;
-            nextEpisode: any;
-            servers: Array<any>;
-            subtitlesJson: string;
-            subtitles: Array<{
-              id: number;
-              name: string;
-              language: string;
-            }>;
-            subtitlesMap: {
-              "1": string;
-              "2": string;
-              "4": string;
-              "6": string;
-            };
-            comments: Array<any>;
-            fonts: Array<string>;
-          };
+    const { data: streamJson } = await sendRequest<{
+      anime: {
+        id: number;
+        titleRomanji: string;
+        titleEnglish: string;
+        titleJapanese: string;
+        synonym: string;
+        synopsis: string;
+        slug: string;
+        year: number;
+        isAdult: boolean;
+        totalEpisodes: number;
+        imgUrl: string;
+        imgBanner: string;
+        trailerLink: string;
+        Type: {
+          id: number;
+          name: string;
         };
+        Status: {
+          id: number;
+          name: string;
+        };
+        Season: {
+          id: number;
+          name: string;
+        };
+        AnimeGenres: Array<{
+          Genre: {
+            id: number;
+            name: string;
+          };
+        }>;
       };
-    };
+      currentEpisode: string;
+      episode: {
+        id: number;
+        title: string;
+        number: number;
+        imgUrl: string;
+        animeId: number;
+        isProcessed: boolean;
+        openingStartsAt: number;
+        openingEndsAt: number;
+        _count: {
+          EpisodeViews: number;
+        };
+        AudioStreams: Array<{
+          id: number;
+          episodeId: number;
+          languageId: number;
+          isDefault: boolean;
+          autoSelect: boolean;
+          playlistUri: string;
+        }>;
+      };
+      episodes: Array<{
+        id: number;
+        title: string;
+        number: number;
+        imgUrl: string;
+        animeId: number;
+        isProcessed: boolean;
+        openingStartsAt: number;
+        openingEndsAt: number;
+        _count: {
+          EpisodeViews: number;
+        };
+        AudioStreams: Array<{
+          id: number;
+          episodeId: number;
+          languageId: number;
+          isDefault: boolean;
+          autoSelect: boolean;
+          playlistUri: string;
+        }>;
+      }>;
+      previousEpisode: any;
+      nextEpisode: any;
+      servers: Array<any>;
+      subtitlesJson: string;
+      subtitles: Array<{
+        id: number;
+        name: string;
+        language: string;
+      }>;
+      subtitlesMap: {
+        "1": string;
+        "2": string;
+        "4": string;
+        "6": string;
+      };
+      comments: Array<any>;
+      fonts: Array<string>;
+    }>(`${anime.baseUrl}/api/episode/${animeId}/${number}`);
 
-    if (!data) return sendResponse(container);
+    if (!json) return sendResponse(container);
 
-    const subtitles = JSON.parse(
-      data?.props?.pageProps?.episodeData?.subtitlesJson
-    ) as Array<{
+    const subtitles = JSON.parse(streamJson?.subtitlesJson) as Array<{
       id: number;
       episodeId: number;
       subtitleId: number;
@@ -380,10 +277,8 @@ const anime: WindowAnime = {
       });
     }
 
-    const openingStartsAt =
-      data?.props?.pageProps?.episodeData?.episode.openingStartsAt;
-    const openingEndsAt =
-      data?.props?.pageProps?.episodeData?.episode.openingEndsAt;
+    const openingStartsAt = streamJson?.episode.openingStartsAt;
+    const openingEndsAt = streamJson?.episode.openingEndsAt;
 
     if (openingEndsAt && openingStartsAt) {
       container.timestamps = [
@@ -460,81 +355,33 @@ const anime: WindowAnime = {
 
     if (query === "null") return [];
 
-    const encodedQuery = encodeURIComponent(query);
-
     const { data } = await sendRequest<{
-      animes: Array<{
+      results: {
         id: number;
-        anilistId: number;
-        titleRomanji: string;
-        titleEnglish: string;
-        titleJapanese: string;
-        titleSpanish: any;
-        titleFilipino: any;
-        titleHindi: any;
-        titleKorean: any;
-        synonym: string;
-        synopsis: string;
-        slug: string;
-        statusId: number;
-        typeId: number;
-        year: number;
-        seasonId: number;
-        totalEpisodes: number;
-        seasonNumber: any;
-        imgUrl: string;
-        imgBanner: string;
-        trailerLink: string;
-        animeCrunchyId: string;
-        crunchyrollId: string;
-        hidiveId: any;
-        seasonHidiveId: any;
-        initialAirDate: string;
-        isAdult: boolean;
-        prequelId: any;
-        sequelId: any;
-        Type: {
-          id: number;
-          name: string;
+        title: {
+          romaji?: string;
+          english?: string;
         };
-        Status: {
-          id: number;
-          name: string;
+        coverImage: {
+          medium: string;
         };
-      }>;
-      page: number;
-      pages: number;
-      genres: Array<{
-        id: number;
-        name: string;
-      }>;
-      years: Array<{
-        year: number;
-      }>;
-      types: Array<{
-        id: number;
-        name: string;
-      }>;
-      status: Array<{
-        id: number;
-        name: string;
-      }>;
-      selectedGenres: Array<any>;
-      selectedYears: Array<any>;
-      selectedTypes: Array<any>;
-      selectedStatus: Array<any>;
-    }>(
-      `${anime.baseUrl}/api/directory?page=1&genres=&years=&types=&status=&title=${encodedQuery}&category=`
-    );
+      }[];
+    }>({
+      url: `${anime.baseUrl}/api/fetchAnime`,
+      data: {
+        query,
+      },
+      method: "POST",
+    });
 
-    if (!data?.animes?.length) return [];
+    if (!data?.results?.length) return [];
 
-    return data.animes.map((anime) => ({
-      id: anime.slug,
-      title: anime.titleEnglish || anime.titleRomanji || anime.titleJapanese,
-      thumbnail: `https://ipfs.animeui.com/ipfs/${anime.imgUrl}`,
+    return data.results.map((anime) => ({
+      id: anime.id.toString(),
+      title: anime.title.romaji || anime.title.english || "",
+      thumbnail: anime.coverImage.medium,
       extra: {
-        anilistId: anime.anilistId.toString(),
+        anilistId: anime.id.toString(),
       },
     }));
   },
